40bede24ba00db528bdb58c6186e5bb97f43ff6a
sayan6969 <121610005+sayan6969@users.noreply.github.com>
2023-01-26 00:43:27 +0530

    #include<stdio.h> int main() {     int n,x,y,c,i;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         int x,y;         scanf("%d%d",&x,&y);         int c;         c=x+y;         printf("%d ",c);     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,x,y,c,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        int x,y;
+        scanf("%d%d",&x,&y);
+        int c;
+        c=x+y;
+        printf("%d
+",c);
+    }
+}
