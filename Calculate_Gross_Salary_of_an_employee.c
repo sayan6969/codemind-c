d55395dd6e8aa47982627371205afa5e98097e4c
sayan6969 <121610005+sayan6969@users.noreply.github.com>
2023-01-26 01:53:02 +0530

    #include<stdio.h> int main() {     float bs,hra,da,pf,gs;     scanf("%f%f%f",&bs,&hra,&da);     pf=0.12*bs;     gs=bs+hra+da+pf;     printf("%0.2f %0.2f",pf,gs);
    }

---
+++ Calculate_Gross_Salary_of_an_employee.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float bs,hra,da,pf,gs;
+    scanf("%f%f%f",&bs,&hra,&da);
+    pf=0.12*bs;
+    gs=bs+hra+da+pf;
+    printf("%0.2f
+%0.2f",pf,gs);
+    
+    
+}
